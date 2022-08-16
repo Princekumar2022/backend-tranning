123e15aa44654aa1ffad90c661a7bb2fc7f1c345
Princekumar2022 <pk279183@gmail.com>
2022-08-16 17:54:48 +0530

    file added

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -14,7 +14,7 @@ const bookSchema = new mongoose.Schema( {
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Book1', bookSchema) //users
 
 //Validation:
 //require:true
