1f813349deabb6ac31efd2d5479e7178699803c5
Princekumar2022 <pk279183@gmail.com>
2022-08-28 14:54:02 +0530

    file updated assignment/auth1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,12 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 const userModel = require("../models/userModel");
 
 
 const createUser = async function (req, res) {
   
   let data = req.body;
-  let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
+  let savedData = await userModel.create(data); 
+  console.log(req.newAtribute);
   res.send({ msg: savedData });
 };
 
@@ -16,7 +16,7 @@ const loginUser = async function (req, res) {
 
   let user = await userModel.findOne({ emailId: userName, password: password });
   if (!user)
-    return res.send({
+    return res.send({ 
       status: false,
       msg: "username or the password is not corerct",
     });
@@ -36,15 +36,13 @@ const loginUser = async function (req, res) {
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-auth-token"];
-
-  // if (!token) return res.send({ status: false, msg: "token must be present" });
-
+  let token = req.headers["x-auth-token"]; 
   console.log(token);  
 
   let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
   if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
+   return res.send({ status: false, msg: "token is invalid" });
+  console.log(decodedToken); 
 
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
@@ -64,10 +62,17 @@ const updateUser = async function (req, res) {
 
   let userData = req.body;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
+  res.send({ status: updatedUser  }); 
+};
+
+const deleteUser = async function (req, res) {
+  let userId = req.params.userId;
+  let deleteUser = await userModel.findOneAndUpdate({ _id: userId }, {$set: {isDeleted: true}},{new:true});
+  return res.send({ status:true, Data: deleteUser });  
 };
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,13 +1,21 @@
-const mid1= function ( req, res, next) {
-
-let token = req.headers["x-Auth-token"];
-    if (!token)token = req.headers["x-auth-token"];
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-  next()
- }
+const jwt = require('jsonwebtoken');
 
 
- 
- 
+const mid1 = function (req, res, next) {
 
-module.exports.mid1= mid1
+   let token = req.headers["x-auth-token"];
+   if (!token) {
+      return res.send({ status: false, msg: "token must be present" });
+   }
+   let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
+   if (decodedToken) {
+      next();
+   } else {
+      return res.send({ status: false, message: 'Token is not valid' })
+   }
+}
+
+
+module.exports.mid1 = mid1
+
+
