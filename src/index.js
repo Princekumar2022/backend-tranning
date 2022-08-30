a7b8b7605b77fec411aeb1897885669ab62d0391
Princekumar2022 <pk279183@gmail.com>
2022-08-30 14:06:55 +0530

    assignment/auth file added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://princekumar2022:ktbaRBP25CkJa0L0@cluster0.kcuyi1y.mongodb.net/pk-22?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
